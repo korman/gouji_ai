a7bbaf15c1a9d135b67ddc027913f3462d67e5b1
Korman <korman@qq.com>
2025-03-20 05:24:35 +0800

    格式化

--- gouji/components/__init__.py
+++ gouji/components/__init__.py
@@ -2,4 +2,5 @@ from .card_components import Card, Hand
 from .player_components import PlayerComponent, TeamComponent
 from .game_components import GameStateComponent
 
-__all__ = ["Card", "Hand", "PlayerComponent", "TeamComponent", "GameStateComponent"]
+__all__ = ["Card", "Hand", "PlayerComponent",
+           "TeamComponent", "GameStateComponent"]
